22d8343d238952307e36fb1bc846be1040eeab16
Undoza6096 <106401980+Undoza6096@users.noreply.github.com>
2022-06-28 15:26:16 -0300

    Update VeproTheory.js

--- VeproTheory.js
+++ VeproTheory.js
@@ -14,8 +14,21 @@ var currency;
 
 var init = () => {
     currency = theory.createCurrency();
+    
+    ///////////////////
+    // Regular Upgrades
+
+    // a1
+    {
+        let getDesc = (level) => "a_1=" + getA1(level).toString(0);
+        a1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(5, Math.log2(2.2))));
+        a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
+        a1.getInfo = (amount) => Utils.getMathTo(getDesc(a1.level), getDesc(a1.level + amount));
+    }
 }
 
 var getTau = () => currency.value;
+
+var getA1 = (level) => Utils.getStepwisePowerSum(level, 3, 10, 0);
 
 init();
