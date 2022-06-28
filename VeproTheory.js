2d295a1e01d6d927586293734c7f013217233577
Undoza6096 <106401980+Undoza6096@users.noreply.github.com>
2022-06-28 15:07:30 -0300

    Add files via upload

---
+++ VeproTheory.js
@@ -0,0 +1,13 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "vepro";
+var name = "Vepro Theory";
+var description = "Ok is based which veprogames.";
+var authors = "Undoza6096";
+var version = "1.0.0";
+
+var currency;
