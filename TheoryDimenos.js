37e2be4cbce2300d4a7c8f631b887382b393c3ea
Undoza6096 <106401980+Undoza6096@users.noreply.github.com>
2022-06-28 21:51:55 -0300

    Add files via upload

---
+++ TheoryDimenos.js
@@ -0,0 +1,19 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
+var id = "theory";
+var name = "Theory Dimenos";
+var description = "i Sorry i do get impossible...";
+var authors = "Undoza6096";
+var version = 1;
+
+var currency;
+
+var init = () => {
+    currency = theory.createCurrency();
+    
+    var getTau = () => currency.value;
+}
