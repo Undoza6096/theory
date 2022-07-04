d3ab03cb6073adf97176d3efc6899a9a12c88893
Undoza6096 <106401980+Undoza6096@users.noreply.github.com>
2022-07-04 15:29:10 -0300

    Update Vebbme.js

--- Vebbme.js
+++ Vebbme.js
@@ -31,7 +31,7 @@ var init = () => {
     {
         let getDesc = (level) => "q_2=1.7^{" + level + "}";
         let getInfo = (level) => "q_2=" + getQ2(level).toString(0);
-        q2 = theory.createUpgrade(0, currency, new ExponentialCost(5, Math.log2(5)));
+        q2 = theory.createUpgrade(1, currency, new ExponentialCost(90, Math.log2(5)));
         q2.getDescription = (_) => Utils.getMath(getDesc(q2.level));
         q2.getInfo = (amount) => Utils.getMathTo(getInfo(q2.level), getInfo(q2.level + amount));
     }
@@ -54,6 +54,7 @@ var getPublicationMultiplier = (tau) => tau.pow(0.1) / BigNumber.THREE;
 var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.1}}{3}";
 
 var getQ1 = (level) => BigNumber.from(1.6).pow(level);
+var getQ2 = (level) => BigNumber.from(1.7).pow(level);
 
 
 init();
